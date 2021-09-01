f6ea71b193b9bd8786fc4e34f692b2763b55832e
khaled <khaledabderrahim512@gmail.com>
2021-09-01 19:02:02 +0200

    implementing mimum requirement the number must be of equal length and add unit test for it

--- src/main/java/algos/khaled/Karatsuba.java
+++ src/main/java/algos/khaled/Karatsuba.java
@@ -1,8 +1,23 @@
 package algos.khaled;
 
-
+/**
+ * @author khaled abderrahim
+ */
 public class Karatsuba {
-    public static void main( String[] args ){ 
+
+    public static void main(String[] args) {
         System.out.println("Karatsuba algorithm implementation:");
+    }
+    /**
+     * Returns the product of {@code first} multiplied by {@code seconde}
+     * @param first the array representation of the first number
+     * @param seconde the array representation of the seconde number
+     * @return first times seconde by applying <a href="https://en.wikipedia.org/wiki/Karatsuba_algorithm">Karatsuba Algorithm</a>
+     * @throws IllegalArgumentException if {@code first} and {@code seconde} are inequal
+     */
+    public static int multiply(int[] first , int[] seconde){
+        if(first.length != seconde.length)
+            throw new IllegalArgumentException("Length inequality first: "+first.length+", seconde: "+seconde.length);
+        return 0;
     }
 }
--- src/test/java/algos/khaled/KaratsubaTest.java
+++ src/test/java/algos/khaled/KaratsubaTest.java
@@ -1,7 +1,19 @@
 package algos.khaled;
 
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+/**
+ * test class for Karatsuba
+ */
 public class KaratsubaTest {
-
-
+    
+    @Test
+    void testCheckEqualLength() {
+        int[] first = {1, 2};
+        int[] seconde = {2};
+        Exception exception = assertThrows(IllegalArgumentException.class, ()->Karatsuba.multiply(first, seconde));
+        String message = exception.getMessage();
+        assertEquals("Length inequality first: "+first.length+", seconde: "+seconde.length, message);
+    }
 }
