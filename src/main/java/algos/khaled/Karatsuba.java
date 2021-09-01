32751c9dc390037ecff49c8a6b4743b8c571c6a3
saladmaker <khaledabderrahim512@gmail.com>
2021-09-02 00:07:14 +0200

    add minimum requirement numbers must of lenght n=2^m where m is natural number, adding unit test to implementation and enhancing documentation

--- src/main/java/algos/khaled/Karatsuba.java
+++ src/main/java/algos/khaled/Karatsuba.java
@@ -17,19 +17,34 @@ public class Karatsuba {
      * @return first times seconde by applying
      * <a href="https://en.wikipedia.org/wiki/Karatsuba_algorithm">Karatsuba
      * Algorithm</a>
-     * @throws IllegalArgumentException if {@code first} and {@code seconde} are
-     * inequal
+     * @throws IllegalArgumentException if {@code first} and {@code seconde} have
+     * inequal length
      * @throws NumberFormatException if {@code first}, {@code seconde} are empty
+     * @throws NumberFormatException if {@code first}'s, {@code seconde}'s lengths
+     * is not <i><em>power of two n= 2^m where m is a natural number</em></i>
      */
     public static int multiply(int[] first, int[] seconde) {
         if (first.length != seconde.length) {
             throw new IllegalArgumentException("Length inequality first: " + first.length + ", seconde: " + seconde.length);
         }
-
-        if (first.length == 0) {
+        int n =first.length;
+        if (n == 0) {
             throw new NumberFormatException("empty number!");
         }
-
+        
+        /*
+        * by checking n &(n-1) we can determin if n is power of two, n = 2^m
+        * where m is natural number for example 8 = 2^3
+        * 8 & 7= 0
+        *   1 0 0 0
+        * + 0 1 1 1
+        *   ========
+        *   0 0 0 0
+        */
+        int checkPowerOfTwo= (n)&(n-1);
+        if(checkPowerOfTwo!=0){
+            throw new NumberFormatException("numbers's length must 2^m where m is natural number 0,1,2,...etc.");
+        }
         return 0;
     }
 }
--- src/test/java/algos/khaled/KaratsubaTest.java
+++ src/test/java/algos/khaled/KaratsubaTest.java
@@ -23,4 +23,18 @@ public class KaratsubaTest {
         String message = exception.getMessage();
         assertEquals("empty number!", message);
     }
+    @Test
+    void testLengthIsPowerOfTwo(){
+        int[] first ={1,2,3};
+        int[] seconde ={1,0,4};
+        Exception exception = assertThrows(NumberFormatException.class, ()->Karatsuba.multiply(first, seconde));
+        String message = exception.getMessage();
+        assertEquals("numbers's length must 2^m where m is natural number 0,1,2,...etc.",message);
+        // make sure this also apply to even length number
+        int[] evenLengthFirst ={1,2,3,4,3,2};
+        int[] evenLengthSeconde ={1,0,4,3,3,8};
+        exception = assertThrows(NumberFormatException.class, ()->Karatsuba.multiply(evenLengthFirst, evenLengthSeconde));
+        message = exception.getMessage();
+        assertEquals("numbers's length must 2^m where m is natural number 0,1,2,...etc.",message);
+    }
 }
